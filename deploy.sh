3ef940bc673475fb76188f6c2fa4db45a7a67296
kenguruka grevisse <kenguruka24@gmail.com>
2018-06-11 12:45:54 +0300

    added more scss

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 echo "$0: publish package @kengres/config";
 echo "start publish..."
+echo "patch version..."
 npm run patch
-npm run publish
+echo "push to git..."
 git push
+echo "publishing..."
+npm run publish
+echo "end..."
